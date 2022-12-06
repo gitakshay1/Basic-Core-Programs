77740e6c4619504ec00022954cb95cd8b6e3680a
gitakshay1 <akshaypatil309p8@gmail.com>
2022-12-06 18:17:54 +0530

    [Akshay]-Add Flip Coin

---
+++ Basic Core Programs/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace Basic_Core_Programs
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            bool Flag=true;
+            while(Flag)
+            {
+                Console.WriteLine("1 Flip Coin");
+                int Option=Convert.ToInt32(Console.ReadLine());
+                switch(Option)
+                {
+                    case 1:
+                        FlipCoin flipCoin = new FlipCoin();
+                        flipCoin.checkFlipCoin();
+                        break;
+                    default:
+                        Console.WriteLine("Enter Valid Number");
+                        break;
+                }
+            }
+        }
+    }
+}
